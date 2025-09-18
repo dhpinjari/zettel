6ec6b1f4e1690cf87d6799ed66335c1a3980d5fc
Danish <dhpinjari@outlook.com>
2025-09-19 00:26:44 +0530

    FinalDesign

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  res.send("working");
+  res.render("zettel");
 });
 
 app.listen(3000);
