27df5d3ded52abb549df5326268a7305e7d0b65e
Danish <dhpinjari@outlook.com>
2025-09-20 00:08:20 +0530

    Login and registration design

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+const mainNotesRoute = require("./routes/mainNotesRoute");
+const registrationRoute = require("./routes/registrationRoute");
+const loginRoute = require("./routes/loginRoute");
+
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -9,4 +13,11 @@ app.get("/", (req, res) => {
   res.render("zettel");
 });
 
+app.get("/registration", (req, res) => {
+  res.render("registration");
+});
+
+app.use("/mainNotes", mainNotesRoute);
+app.use("/login", loginRoute);
+app.use("/registration", registrationRoute);
 app.listen(3000);
