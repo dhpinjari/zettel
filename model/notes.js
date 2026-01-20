49c052f89ba11bb4bc98e8a3f1d3752b202b4bb3
danish <dhpinjari@yahoo.com>
2026-01-20 17:44:31 +0530

    20012026-AddTaskUi

---
+++ model/notes.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const noteSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Please add a note title"],
+      trim: true,
+      maxlength: [100, "Title cannot be more than 100 characters"],
+    },
+    mainNote: {
+      type: String,
+      required: [true, "Please add the note content"],
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // Ensures relationship to the User model
+      required: true,
+      index: true, // Optimizes queries for a specific user's notes
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("Note", noteSchema);
